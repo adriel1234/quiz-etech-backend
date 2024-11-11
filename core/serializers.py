1139d347b1c443832770c79713395dc0edc4a5a1
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-11-10 22:59:18 -0400

    add serializers.py

---
+++ core/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from core import models
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Question
+        exclude = ['created_at']
+
+
+class OptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Option
+        exclude = ['created_at']
+
+
+class QuestionGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.QuestionGroup
+        exclude = ['created_at']
+
+
+class QuestionGroupQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.QuestionGroupQuestion
+        exclude = ['created_at']
+
+
+class MatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Match
+        exclude = ['created_at']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User
+        exclude = ['created_at', 'password']
+
+
+class MatchUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.MatchUser
+        exclude = ['created_at']
