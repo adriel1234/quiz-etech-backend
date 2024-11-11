a30102f91627b039e13fe821c633286267a8d836
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-11-10 23:05:34 -0400

    add urls.py e edit urls.py quiz

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from rest_framework import routers
+from core import viewsets
+
+router = routers.DefaultRouter()
+
+router.register('question', viewsets.QuestionViewSet)
+router.register('option', viewsets.OptionViewSet)
+router.register('question-group', viewsets.QuestionGroupViewSet)
+router.register('question-group-question', viewsets.QuestionGroupQuestionViewSet)
+router.register('match', viewsets.MatchViewSet)
+router.register('user', viewsets.UserViewSet)
+router.register('match-user', viewsets.MatchUserViewSet)
+
+urlpatterns = router.urls
