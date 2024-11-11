ff7c89d6444f4285b94e5f0f05ccb8d771b66542
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-11-10 23:01:11 -0400

    add viewsets.py

---
+++ core/viewsets.py
@@ -0,0 +1,44 @@
+from rest_framework import viewsets, permissions
+from core import models, serializers, filters
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = models.Question.objects.all()
+    serializer_class = serializers.QuestionSerializer
+    filterset_class = filters.QuestionFilter
+
+
+class OptionViewSet(viewsets.ModelViewSet):
+    queryset = models.Option.objects.all()
+    serializer_class = serializers.OptionSerializer
+    filterset_class = filters.OptionFilter
+
+
+class QuestionGroupViewSet(viewsets.ModelViewSet):
+    queryset = models.QuestionGroup.objects.all()
+    serializer_class = serializers.QuestionGroupSerializer
+    filterset_class = filters.QuestionGroupFilter
+
+
+class QuestionGroupQuestionViewSet(viewsets.ModelViewSet):
+    queryset = models.QuestionGroupQuestion.objects.all()
+    serializer_class = serializers.QuestionGroupQuestionSerializer
+    filterset_class = filters.QuestionGroupQuestionFilter
+
+
+class MatchViewSet(viewsets.ModelViewSet):
+    queryset = models.Match.objects.all()
+    serializer_class = serializers.MatchSerializer
+    filterset_class = filters.MatchFilter
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+    filterset_class = filters.UserFilter
+
+
+class MatchUserViewSet(viewsets.ModelViewSet):
+    queryset = models.MatchUser.objects.all()
+    serializer_class = serializers.MatchUserSerializer
+    filterset_class = filters.MatchUserFilter
