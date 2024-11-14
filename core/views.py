bd858fa2a027a4e62943114151095b34c2c9679f
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-11-14 10:11:00 -0400

    add autenticação jwt

--- core/views.py
+++ core/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+class RegisterView(APIView):
+    permission_classes = [AllowAny]  # Permite acesso sem autenticação
+
+    def post(self, request):
+        name = request.data.get("name")
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        if User.objects.filter(username=email).exists():
+            return Response({"error": "User already exists"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(username=email, email=email, password=password)
+        user.first_name = name
+        user.save()
+
+        refresh = RefreshToken.for_user(user)
+        return Response({"token": str(refresh.access_token), "name": user.first_name}, status=status.HTTP_201_CREATED)
--- quiz/settings.py
+++ quiz/settings.py
@@ -127,12 +127,20 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
-        # 'rest_framework.filters.OrderingFilter',
-    ),
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    ],
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+# APPEND_SLASH = False
+
