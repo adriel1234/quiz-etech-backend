c955bc26331a25831f40afdee5cdf70b789942a4
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-11-23 19:00:34 -0400

    remove jwt

--- core/viewsets.py
+++ core/viewsets.py
@@ -7,39 +7,39 @@ class QuestionViewSet(viewsets.ModelViewSet):
     queryset = models.Question.objects.all()
     serializer_class = serializers.QuestionSerializer
     filterset_class = filters.QuestionFilter
-    permission_classes = [permissions.IsAuthenticated]
+
 
 
 class OptionViewSet(viewsets.ModelViewSet):
     queryset = models.Option.objects.all()
     serializer_class = serializers.OptionSerializer
     filterset_class = filters.OptionFilter
-    permission_classes = [permissions.IsAuthenticated]
+
 
 
 class QuestionGroupViewSet(viewsets.ModelViewSet):
     queryset = models.QuestionGroup.objects.all()
     serializer_class = serializers.QuestionGroupSerializer
     filterset_class = filters.QuestionGroupFilter
-    permission_classes = [permissions.IsAuthenticated]
+
 
 
 class MatchViewSet(viewsets.ModelViewSet):
     queryset = models.Match.objects.all()
     serializer_class = serializers.MatchSerializer
     filterset_class = filters.MatchFilter
-    permission_classes = [permissions.IsAuthenticated]
+
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
     filterset_class = filters.UserFilter
-    permission_classes = [permissions.IsAuthenticated]
+
 
 
 class MatchUserViewSet(viewsets.ModelViewSet):
     queryset = models.MatchUser.objects.all()
     serializer_class = serializers.MatchUserSerializer
     filterset_class = filters.MatchUserFilter
-    permission_classes = [permissions.IsAuthenticated]
+
--- quiz/urls.py
+++ quiz/urls.py
@@ -22,9 +22,9 @@ from core.views import RegisterView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/api-auth/', include('rest_framework.urls')),
-    path('api/token/register/', RegisterView.as_view(), name='register'),
+    # path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/api-auth/', include('rest_framework.urls')),
+    # path('api/token/register/', RegisterView.as_view(), name='register'),
     path('api/', include('core.urls')),
 ]
