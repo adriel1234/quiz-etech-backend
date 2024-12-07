b1148c3022a5335f870116b9e6cc7b34db3c412a
adriel <adriel1234.1234@gmail.com>
2024-12-06 20:04:32 -0400

    limite do token 7 dias

--- quiz/settings.py
+++ quiz/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from os.path import exists
 from dotenv import load_dotenv
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -162,3 +163,7 @@ CORS_ALLOW_HEADERS = [
 
 # APPEND_SLASH = False
 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+}
